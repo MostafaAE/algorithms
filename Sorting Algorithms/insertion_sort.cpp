354ed64729b78f86342d14fb3e6cf0f0b813a7a3
MostafaAE <mostafa.aymanae@gmail.com>
2022-10-20 17:23:04 +0200

    insertion sort v2

--- Sorting Algorithms/insertion_sort.cpp
+++ Sorting Algorithms/insertion_sort.cpp
@@ -8,7 +8,7 @@ using namespace std;
  */
 void insertion_sort(vector<int> &nums)
 {
-    // For each number:add it in the previous sorted sub-array
+    // For each number:a dd it in the previous sorted sub-array
     for (int i = 1; i < (int)nums.size(); i++)
     {
         int key = nums[i];
@@ -24,6 +24,24 @@ void insertion_sort(vector<int> &nums)
     }
 }
 
+/*
+ * This version sorts the elements in descending order
+ * and utilizes the swap functionto reduce the code length, but in return it
+ * performs more operations than the first version if the swap operation takes ~3 steps
+ *
+ * Complexity:
+ * Time Complexity : O(n^2)
+ * Space Complexity : O(1)
+ */
+void insertion_sort_v2(vector<int> &nums)
+{
+    // For each element: keep swapping it with the previous until it reaches
+    // its right position in the previous sorted sub-array
+    for (int i = 1; i < (int)nums.size(); i++)
+        for (int j = i - 1; j >= 0 && nums[j] < nums[j + 1]; j--)
+            swap(nums[j], nums[j + 1]);
+}
+
 void print_vec(const vector<int> &nums)
 {
     for (int num : nums)
@@ -31,15 +49,29 @@ void print_vec(const vector<int> &nums)
     cout << "\n";
 }
 
-int main()
+void test_insertion_sort()
 {
     vector<int> nums{5, 4, 3, 6, 2, 1, 7, 10, 9, 8, 2, 3, 4};
-
     print_vec(nums); // 5 4 3 6 2 1 7 10 9 8 2 3 4
 
     insertion_sort(nums);
 
     print_vec(nums); // 1 2 2 3 3 4 4 5 6 7 8 9 10
+}
+
+void test_insertion_sort_v2()
+{
+    vector<int> nums{5, 4, 3, 6, 2, 1, 7, 10, 9, 8, 2, 3, 4};
+    print_vec(nums); // 5 4 3 6 2 1 7 10 9 8 2 3 4
+
+    insertion_sort_v2(nums);
+
+    print_vec(nums); // 10 9 8 7 6 5 4 4 3 3 2 2 1
+}
+int main()
+{
+    test_insertion_sort();
+    test_insertion_sort_v2();
 
     // must see it, otherwise RTE
     cout << "\n\nNO RTE\n";
